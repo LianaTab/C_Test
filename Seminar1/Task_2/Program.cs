565c28c6fdd28a64f500776ac4a699c2a583f763
Liana <liana.tabaka@gmail.com>
2022-08-20 17:27:17 +0300

    add seminar1

---
+++ Seminar1/Task_2/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт максимальное.
+
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3 */
+
+/* int a = 5, b = 7;
+int max = a;
+if (a > b) max = a;
+if (a < b) max = b;
+
+Console.WriteLine(max); */
+
+
+/* int a = 2, b = 10;
+int max = a;
+if (a > b) max = a;
+if (a < b) max = b;
+
+Console.WriteLine(max); */ 
+
+int a = -9, b = -3;
+int max = a;
+if (a > b) max = a;
+if (a < b) max = b;
+
+Console.WriteLine(max);
