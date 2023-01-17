327bca658a514351e284278f7d5d8c1f65a9e309
Liana <liana.tabaka@gmail.com>
2023-01-17 23:51:34 +0300

    Save changes

---
+++ Seminar1/Seminar_task1/Program.cs
@@ -0,0 +1,9 @@
+﻿// Написать программу, которая на вход принимает число и выдаёт его квадрат (число, умноженное само на себя)
+Console.WriteLine ("Возведение числа в квадрат");
+Console.WriteLine("Пожалуйста, введите число и мы возведём его в квадрат");
+int number = int.Parse(Console.ReadLine());
+//или int number = Convert.ToInt32(Consol.ReadLine());
+Console.WriteLine("Введено число " + number);
+
+int result = number * number;
+Console.WriteLine("Квадрат числа " + number + " есть число " + result);
---
+++ Seminar1/Seminar_task2/Program.cs
@@ -0,0 +1,22 @@
+﻿//  Напишите программу, которая на вход принимает два числа и проверяет, является ли первое квадратом второго
+
+Console.WriteLine("Проверка, является ли число 1 квадратом числа 2");
+
+int number1 = int.Parse(Console.ReadLine());
+int number2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Пожалуйста, введите число 1");
+int number1 = Console.ReadLine();
+
+Console.WriteLine("Пожалуйста, введите число 2");
+int number2 = Console.ReadLine();
+
+if (number1 = number2 * number2)
+{
+Console.WriteLine("Первое число является квадратом второго");
+}
+
+else 
+{
+Console.WriteLine("Первое число НЕ является квадратом второго");
+}
