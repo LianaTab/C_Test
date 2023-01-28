c0c9085f7f0fd624f46882689d4a4456dd80d2a7
stagelight <stagelight@MacBook-Pro-stagelight.local>
2023-01-28 21:09:04 +0300

    Task11

---
+++ C_Test/Seminar2_Tasks/Task_11/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+11. Напишите программу, которая выводит случайное трёхзначное число 
+и удаляет вторую цифру этого числа
+*/
+
+int number = new Random().Next(100, 1000);
+
+ Console.WriteLine("Сгенерировано число " + number);
+
+ int firstDigit = number / 100;
+ int thirdDigit = number % 10;
+
+Console.WriteLine("Получилось число: " + firstDigit + thirdDigit);
+
+
+// Или через доп переменную result
