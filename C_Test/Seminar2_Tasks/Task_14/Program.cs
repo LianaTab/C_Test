383246cf3f477a2d124e40c1bbd3a16b3791c676
stagelight <stagelight@MacBook-Pro-stagelight.local>
2023-01-28 21:56:53 +0300

    Task14

---
+++ C_Test/Seminar2_Tasks/Task_14/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Напишите программу, которая принимает на вход число и проверяет, кратно
+ли оно одновременно 7 и 23
+
+14 - нет
+46 - нет
+161 - да
+*/
+
+
+Console.WriteLine("Пожалуйста, введите число");
+int num = int.Parse(Console.ReadLine());
+
+
+if (num == 0)
+{
+System.Console.WriteLine("Число является нулём");
+return;
+}
+
+int firstTask = (num % 7);
+int secondTask = (num % 23);
+
+if (firstTask == 0 && secondTask == 0)
+Console.WriteLine("Число кратно 7 и 23");
+
+else
+Console.WriteLine("Введённое число НЕ кратно 7 и 23");
+
--- C_Test/Seminar2_Tasks/Task_9/Program.cs
+++ C_Test/Seminar2_Tasks/Task_9/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* 9. Напишите программу, которая выводит случайное число из отрезка [10, 99 ]
+и показывает наибольшую цифру цисла 
+78 - 8
+12 - 2
+85 - 8
+*/
+
+/*   "Защита от дурака", проверка ввода в консоль верного формата информации.
+Первый оператор возвращает boolean
+Чтобы в консоль не выводились сообщения об ошибках.
+
+if(int.TryParse(Console.ReadLine(), out int number))
+    Console.WriteLine("Прочитали число: " + number);
+else
+     Console.WriteLine("Ввод неверный. Пожалуйста, введите корректное число");
+*/
+
+ int number = new Random().Next(10, 100);
+
+ Console.WriteLine("Сгенерировано число " + number);
+
+ int firstDigit = number / 10;
+ int secondDigit = number % 10;
+
+ /* Тернарный оператор:
+  Console.WriteLine("Наибольшая цифра в числе " + number + ": " + (firstDigit > secondDigit ? firstDigit : secondDigit));
+
+ */
+
+ int maxDigit = 0;
+
+ if (firstDigit > secondDigit)
+    maxDigit = firstDigit;
+    else
+    maxDigit = secondDigit;
+
+    Console.WriteLine("Наибольшая цифра в числе " + number + ": " + maxDigit);
