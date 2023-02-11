a3d492d4ff7bb2dd4b5da9f340d2d443767eef5d
Liana Tabaka <liana.tabaka@gmail.com>
2023-02-11 15:42:23 +0300

    Task_22

---
+++ C_Test/Seminar3_Tasks/Task_22/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Напишите программу, которая принимает на вход число N и выдаёт таблицу квадратов 
+чисел от 1 до N
+5 - 1, 4,9,16,25
+2 - 1,4
+*/
+
+Console.WriteLine("Программа для вывода таблицы квадратов от 1 до заданного числа ");
+Console.WriteLine("Введите число: ");
+int.TryParse(Console.ReadLine(), out int x);
+double Func(int x)
+{
+    return Math.Pow(x, 2);
+}
+
+int i = 1;
+
+while(i <= x)
+{
+    Console.Write(Func(i));
+    if (i<x)
+    Console.Write(", ");
+    i++;
+}
+
