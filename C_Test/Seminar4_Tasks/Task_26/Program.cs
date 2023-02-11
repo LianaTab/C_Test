81d6b4170f7b2c1eead5f4c8ffc11e8ded1235ac
Liana Tabaka <liana.tabaka@gmail.com>
2023-02-11 18:08:29 +0300

    Task_28

---
+++ C_Test/Seminar4_Tasks/Task_26/Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+Напишите программу, которая принимает на вход число 
+и выдаёт количество цифр в числе
+
+456 - 3
+78 - 2
+89126 - 5
+*/
+
+int NumberDigits(int num)
+{
+    if (num < 1)
+    num = Math.Abs(num); // можно оставить return -1, но это вариант для положительных чисел
+    int ind = 0;
+    while (num > 0)
+    {
+        num /= 10;
+        ind++;
+    }
+    return ind;
+}
+
+Console.WriteLine("Программа для вывода количества цифр в заданном числе");
+Console.WriteLine("Введите число: ");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(NumberDigits(num));
+
+/*
+int CountNumbersN(int n)
+{
+    if (n == 0)
+    return 1;
+
+    int result = 0;
+    result = (int)Math.Log10(Math.Abs(n)) + 1;
+}
+*/
---
+++ C_Test/Seminar4_Tasks/Task_28/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+Напишите программу, которая принимает на вход число 
+N и выдаёт произведение чисел от 1 до N.
+
+4 - 24
+5 - 120
+*/
+
+int N = getVar();
+System.Console.WriteLine( multipler(N) );
+
+int multipler(int num)
+{
+    int outNum = 1;
+    
+    for (int i = 1; i < num + 1; outNum *= i++)
+    {}
+
+    return outNum;
+}
+
+int getVar()
+{
+    int varValue = 0;
+    bool isNumeric = false;
+
+    while (!isNumeric)
+    {
+        Console.Write($"Введите число: ");
+        isNumeric = int.TryParse(Console.ReadLine(), out varValue);
+       
+    }
+
+    return varValue;
+}
