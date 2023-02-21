5bb58bce1c860b48461017dbdbacd7684ae2c91c
Liana Tabaka <liana.tabaka@gmail.com>
2023-02-21 19:40:30 +0300

    Task_33

--- Seminar5_Tasks/Task_33/Program.cs
+++ Seminar5_Tasks/Task_33/Program.cs
@@ -6,3 +6,45 @@
 3; массив [6, 7, 19, 345, 3] - да.
 
 */
+
+int checkNum = getVar();
+int[] chekerNumsArr = generateArr(10, 0, 9);
+bool flag = checkArr(checkNum, chekerNumsArr);
+
+System.Console.WriteLine($"Число {checkNum} в массиве {arrToStr(chekerNumsArr)} {(flag ? "" : "не ")}встречается");
+
+int getVar(){
+    int varValue = 0;
+    bool isNumeric = false;
+
+    while (!isNumeric) {
+        Console.Write($"Введите проверяемое число от 0 до 9: ");
+        isNumeric = int.TryParse(Console.ReadLine(), out varValue);
+        varValue = Math.Abs(varValue);
+        if (varValue < 0) {
+            isNumeric = false;
+        }
+    }
+    return varValue;
+}
+
+    bool checkArr(int checkNum, int[] chekerNumsArr) {
+        foreach (var item in chekerNumsArr) {
+            if (item == checkNum) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    int [] generateArr(int n, int min, int max) {
+    int[] arrNum = new int[n];
+    Random rnd = new Random();
+    for (int i = 0; i < arrNum.Length; arrNum[i++] = rnd.Next(min, max +1)) {}
+
+    return arrNum; 
+    }
+
+string arrToStr(int[] nums){
+    return $"[{String.Join("," , nums)}]";
+}
