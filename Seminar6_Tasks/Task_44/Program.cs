a3fd69dc2cb4af0922f430779bce3a3f59f2ca80
Liana Tabaka <liana.tabaka@gmail.com>
2023-02-24 20:36:44 +0300

    Task_44,45

---
+++ Seminar6_Tasks/Task_44/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Не используя рекурсию, выведите первые N чисел Фибоначчи. 
+Первые два числа Фибоначчи 0 и 1.
+
+Если N = 5 -> 0 1 1 2 3 
+Если N = 3 -> 0 1 1 
+Если N = 7 -> 0 1 1 2 3 5 8
+
+*/
+
+System.Console.WriteLine("Введите число: ");
+int input = 0;
+int.TryParse(Console.ReadLine(), out input);
+
+int[] FiboArray(int input) {
+    int[] result = new int[input];
+    result[0] = 0;
+    result[1] = 1;
+    for(int i = 2; i < input; i++) {
+        result[i] = result[i-1] + result[i-2];
+    }
+    return result;
+}
+
+void printArr(int[] nums) {
+    System.Console.Write($"{String.Join(" ", nums)}\n");
+}
+
+printArr(FiboArray(input));
---
+++ Seminar6_Tasks/Task_45/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+Напишите программу, которая будет создавать копию заданного массива 
+с помощью поэлементного копирования.
+*/
+
+int[] CopyArray(int[] inpArr) {
+    int[] result = new int[inpArr.Length];
+    for(int i = 0; i < result.Length; i++) {
+        result[i] = inpArr[i];
+    }
+    return result;
+}
+int[] GenerateArray(int array_Size) {
+    int[] result = new int[array_Size];
+    Random tmp = new Random();
+    for(int i = 0; i < array_Size; i++) {
+        result[i] = tmp.Next(100, 1000);
+    }
+    return result;
+
+}
+void printArr(int[] nums) {
+    System.Console.Write($"{String.Join(" ", nums)}\n");
+
+}
+
+System.Console.WriteLine("Введите размер массива");
+int input = 0;
+int.TryParse(Console.ReadLine(), out input);
+int[] arr1 = GenerateArray(input);
+System.Console.WriteLine("Исходный массив: ");
+printArr(arr1);
+int[] copy = CopyArray(arr1);
+System.Console.WriteLine("Копия массива: ");
+printArr(copy);
